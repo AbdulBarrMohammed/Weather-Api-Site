9a90eb601bb44ae31b47c879a64b505ab689c670
Barr Mohammed <barrmohammed@Barrs-MacBook-Air.local>
2024-05-30 16:09:47 -0400

    done

---
+++ src/createForecastCards.js
@@ -0,0 +1,79 @@
+import sunny from './img/sun.png';
+import thunderStorm from './img/thunderstorm.png';
+import partlyCloudy from './img/cloudy.png';
+import cloudy from './img/clouds.png';
+import rain from './img/storm.png';
+
+
+function createCard(maxTempC, minTempC, maxTempF, minTempF, day, weather, tempType) {
+    const forecastCard = document.createElement('div');
+    forecastCard.style.display = 'flex';
+    forecastCard.style.flexDirection = 'column';
+    forecastCard.style.justifyContent = 'center';
+    forecastCard.style.alignItems = 'center';
+    forecastCard.style.padding = '10px';
+    forecastCard.style.backgroundColor = '#232B3B';
+    forecastCard.style.rowGap = '30px';
+    forecastCard.style.paddingTop = '15px';
+    forecastCard.style.paddingBottom = '15px';
+    forecastCard.style.borderRadius = '10px';
+
+    const pDate = document.createElement('p');
+    var dob = new Date(day);
+    var dobArr = dob.toDateString().split(' ');
+    var currDate = dobArr[2] + ' ' + dobArr[1] + ' ' + dobArr[3];
+    pDate.textContent = currDate;
+    forecastCard.appendChild(pDate);
+
+    const weatherPic = document.createElement('img');
+    if (weather === "Partly cloudy") {
+        weatherPic.src = partlyCloudy;
+    }
+    else if (weather === 'Clear' || weather === 'Sunny') {
+        weatherPic.src = sunny;
+    }
+    else if (weather === 'Overcast') {
+        weatherPic.src = cloudy;
+    }
+    else if (weather.includes('thunder')) {
+        weatherPic.src = thunderStorm;
+    }
+    else if (weather.includes('rain')) {
+        weatherPic.src = rain;
+    }
+    else if (weather === 'Cloudy') {
+        weatherPic.src = cloudy;
+    }
+    else {
+        weatherPic.src = cloudy;
+    }
+    forecastCard.appendChild(weatherPic);
+
+    const highLowDiv = document.createElement('div');
+    highLowDiv.style.display = 'flex';
+    highLowDiv.style.justifyContent = 'space-between';
+    highLowDiv.style.columnGap = '20px';
+
+    const pHigh = document.createElement('p');
+    const pLow = document.createElement('p');
+    let temperature ="\u00B0";
+
+    if (tempType == 'F') {
+        pHigh.textContent = `${maxTempF}${temperature}F`;
+        pLow.textContent = `${minTempF}${temperature}F`;
+    }
+    else {
+        pHigh.textContent = `${maxTempC}${temperature}C`;
+        pLow.textContent = `${minTempC}${temperature}C`;
+    }
+
+    highLowDiv.appendChild(pHigh);
+    highLowDiv.appendChild(pLow);
+
+    forecastCard.appendChild(highLowDiv);
+
+    return forecastCard;
+}
+
+
+export {createCard};
--- src/index.js
+++ src/index.js
@@ -1,15 +1,37 @@
 import "./style.css";
 import { currCityInfo } from "./changeCityInfo";
+import { createCard } from "./createForecastCards";
+//npx webpack --watch
 
 const request = 'http://api.weatherapi.com/v1/forecast.json?key=a9cede541c974464a2e182622242705&q=new york&days=5';
 
 const searchBtn = document.querySelector('.search-btn');
 const cBtn = document.querySelector('.cel');
 const fBtn = document.querySelector('.fah');
+const forecastContainer = document.querySelector('.forecast-containers');
 let tempType = 'C';
 let currInput = 'indianapolis';
 
 getCityWeather('indianapolis');
+getForecast(currInput, tempType)
+
+async function getForecast(input, tempType) {
+    const response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=a9cede541c974464a2e182622242705&q=${input}&days=5`, {mode: 'cors'});
+    const forecastData = await response.json();
+    const forecastLst = forecastData.forecast.forecastday;
+    forecastContainer.innerHTML = '';
+    for (let i = 0; i < forecastLst.length; i++) {
+        const forecastDay = forecastLst[i].day;
+        const maxTempC = forecastDay.maxtemp_c
+        const minTempC = forecastDay.mintemp_c;
+        const maxTempF = forecastDay.maxtemp_f;
+        const minTempF = forecastDay.mintemp_f;
+        const day = forecastLst[i].date;
+        const weather = forecastDay.condition.text
+        forecastContainer.appendChild(createCard(maxTempC, minTempC, maxTempF, minTempF, day, weather, tempType));
+    }
+}
+getForecast(currInput);
 
 async function getCityWeather(input) {
     const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=a9cede541c974464a2e182622242705&q=${input}`, {mode: 'cors'});
@@ -24,14 +46,17 @@ async function getCityWeather(input) {
     const currVisibility = cityData.current.vis_miles;
     const currPressure = cityData.current.pressure_mb;
 
-
-    const currDate = '06-29-2024';
+    var dob = new Date();
+    var dobArr = dob.toDateString().split(' ');
+    var currDate = dobArr[2] + ' ' + dobArr[1] + ' ' + dobArr[3];
 
     if (tempType === 'F') {
         currCityInfo(currCity, tempF, currCondition, currDate, currWindStatus, currHumidity, currVisibility, currPressure, tempType);
+        getForecast(currInput, tempType);
       }
       else {
         currCityInfo(currCity, tempC, currCondition, currDate, currWindStatus, currHumidity, currVisibility, currPressure, tempType);
+        getForecast(currInput, tempType);
       }
 
   }
@@ -39,20 +64,19 @@ async function getCityWeather(input) {
 cBtn.addEventListener('click', function(e) {
     tempType = 'C';
     getCityWeather(currInput);
+    getForecast(currInput, tempType);
 
     cBtn.style.backgroundColor = 'white';
     cBtn.style.color = 'black';
 
     fBtn.style.backgroundColor = '#232B3B';
     fBtn.style.color = 'white';
-
-
 })
-
 
 fBtn.addEventListener('click', function(e) {
     tempType = 'F';
     getCityWeather(currInput);
+    getForecast(currInput, tempType);
     fBtn.style.backgroundColor = 'white';
     fBtn.style.color = 'black';
 
